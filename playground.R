a13e3ced1926f5ac0aa37c533c3f3e52c2f62c08
wulucy <ltwu15@gmail.com>
2019-04-28 20:44:58 -0400

    Initial commit.

---
+++ playground.R
@@ -0,0 +1,12 @@
+install.packages("RSQLite")
+library(RSQLite)
+
+filename <- "billboard-200.db"
+sqlite.driver <- dbDriver("SQLite")
+db <- dbConnect(sqlite.driver,
+                dbname = filename)
+
+## Some operations
+tables <- dbListTables(db)
+acoustic.tb <- dbReadTable(db,"acoustic_features")
+albums.tb <- dbReadTable(db,"albums")
