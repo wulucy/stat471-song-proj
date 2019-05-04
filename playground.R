ab38f6fa45644e5d18b5accc73a4470b98ce9a81
wulucy <ltwu15@gmail.com>
2019-05-04 19:52:31 -0400

    Add season factor

--- playground.R
+++ playground.R
@@ -60,7 +60,7 @@ albums.tb <- dbReadTable(db, "albums")
 bill100 <- read.csv(file = "Hot Stuff.csv")
 colnames(bill100)[4] <- "song"
 colnames(bill100)[5] <- "artist"
-unique(bill100$Song)
+#unique(bill100$Song)
 
 # Merging with World Data
 merged.tb <- merge(bill100, acoustic.tb, by=c("artist", "song"))
@@ -97,3 +97,19 @@ merged.tb2 <- merged.tb %>%
 df2000 <- as.data.frame(subset(merged.tb2, format(WeekID,"%Y")>=2000))
 df2000 <- df2000[order(df2000$WeekID), ]
 dim(df2000)
+
+# Create season factor
+# Winter = 12, 1, 2; Spring = 3, 4, 5, Summer = 6,7,8 Fall = 9, 10, 11
+df2000.seaons <- df2000 %>%
+  mutate(Month=as.numeric(format(WeekID, "%m"))) %>%
+  mutate(Season=
+           case_when(
+             Month == 12 | Month <= 2 ~ "Winter",
+             Month >= 3 & Month <= 5 ~ "Spring",
+             Month >= 6 & Month <= 8 ~ "Summer",
+             Month >= 9 & Month <= 11 ~ "Fall",
+             TRUE ~ "NA"
+               )
+  )
+
+  
