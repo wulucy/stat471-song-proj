2dfacc677dbcf851de0b1001a5c430ed054a4bd9
Duong Nguyen <ngduong@wharton.upenn.edu>
2019-05-04 16:12:27 -0400

    Update playground.R

--- playground.R
+++ playground.R
@@ -1,4 +1,3 @@
-install.packages("RSQLite")
 library(RSQLite)
 
 filename <- "billboard-200.db"
@@ -11,4 +10,15 @@ tables <- dbListTables(db)
 acoustic.tb <- dbReadTable(db,"acoustic_features")
 albums.tb <- dbReadTable(db,"albums")
 
-# commiting
+# Sort by time and date
+merged.tb <- merge(acoustic.tb, albums.tb, by=c("artist", "album"))
+
+# Sort by rank
+sorted <- merged.tb[order(merged.tb$rank, merged.tb$date.y), ]
+clean.sorted <- sorted[, c(-3, -18, -19, -20, -23, -24)]
+
+colnames(clean.sorted)[17] <- "date"
+names(clean.sorted)
+
+# Subset of 2017
+sub2017 <- subset(clean.sorted, format(as.Date(date),"%Y")==2017)
