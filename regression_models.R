3d605414d31dec8f47aaf9efa21ca9fb91d7638a
Andrew Zheng <andrew.a.zheng@gmail.com>
2019-05-05 17:34:15 -0400

    Update regression_models.R

--- regression_models.R
+++ regression_models.R
@@ -1,8 +1,17 @@
 df2000_grouped <- read.csv('df2000_grouped_morefactors.csv')
+df2000_extra <- read.csv('df2000_withgenre_withyear.csv')
 
 # Linear regression - AZ
-lm_df <- df2000_grouped %>%
-  select(-artist, -song, -WeekID, -Weeks.on.Chart, -X)
+lm_df <- df2000_extra %>%
+  select(-artist, -song, -WeekID, -Weeks.on.Chart, -X.1, -X)
+
+lm_df$Year <- as.factor(lm_df$Year)
+
+summary(lm_df)
+
+# Remove the columns that have no or very few observations
+lm_df <- lm_df %>%
+  select(-instrumental, -bluegrass, -jazz, -classical, -broadway, -opera, -hip.hop)
 
 set.seed(22)
 train_indices <- sample(nrow(lm_df), nrow(lm_df) * 0.7, replace=TRUE)
@@ -17,10 +26,10 @@ fit.square <- lm(Peak.Position^2 ~. , lm_df)
 
 # Normal Q-Q Plots
 par(mfrow = c(2,2))
-plot(fit.linear, 2)
-plot(fit.log, 2)
-plot(fit.inverse, 2)
-plot(fit.square, 2)
+plot(fit.linear, 2, main = "No Transformation")
+plot(fit.log, 2, main = "Log Transformation")
+plot(fit.inverse, 2, main = "Log+Inverse Transformation")
+plot(fit.square, 2, main = "Square Transformation")
 
 # Histograms of transformed distribution
 par(mfrow = c(2,2))
@@ -29,40 +38,35 @@ hist(I(log(lm_df$Peak.Position)))
 hist(I(log(101 - lm_df$Peak.Position)))
 hist(I(lm_df$Peak.Position^2))
 
-
 # Add transformed variable to new data frame
 lm_df_1 <- lm_df
 lm_df_1$Peak.Transformed <- log(101 - lm_df$Peak.Position)
 lm_df_1$Peak.Position <- NULL
 
-
 library(leaps)
 
-# Exhaustive model selection with regsubsets returns model with all variables
-fit.exh <- regsubsets(Peak.Transformed ~ ., lm_df_1, nvmax=25, method="exhaustive")
+# Backwards model selection with regsubsets (don't include Year)
+fit.exh <- regsubsets(Peak.Transformed ~ . -Year, lm_df_1, nvmax=25, method="backward")
 f.e <- summary(fit.exh)
-which.min(f.e$rss)
+which.min(f.e$cp)
 data.frame(variables = (1:length(f.e$rsq)),
            r_squared = f.e$rsq,
            rss = f.e$rss,
            bic = f.e$bic,
            cp = f.e$cp)
 
-# Perform manual backwards selection until all variables are significant
-fit.backwards <- lm(Peak.Transformed ~., lm_df_1)
-fit.backwards.1 <- lm(Peak.Transformed ~. -key, lm_df_1)
-fit.backwards.2 <- lm(Peak.Transformed ~. -key -liveness, lm_df_1)
-fit.backwards.3 <- lm(Peak.Transformed ~. -key -liveness -tempo, lm_df_1)
-fit.backwards.4 <- lm(Peak.Transformed ~. -key -liveness -tempo -Season, lm_df_1)
-fit.backwards.5 <- lm(Peak.Transformed ~. -key -liveness -tempo -Season -Month, lm_df_1)
-fit.backwards.6 <- lm(Peak.Transformed ~. -key -liveness -tempo -Season -Month -time_signature, lm_df_1)
-fit.backwards.7 <- lm(Peak.Transformed ~. -key -liveness -tempo -Season -Month -time_signature -loudness, lm_df_1)
-fit.backwards.8 <- lm(Peak.Transformed ~ acousticness + danceability + duration_ms + duration_ms + instrumentalness
-                      + mode + speechiness + valence + artist.pop, lm_df_1)
-summary(fit.backwards.8)
+# Model with 11 variables minimized cp. Remove variables until all are significant
+fit.backwards <- lm(Peak.Transformed ~ danceability + duration_ms + instrumentalness + valence +
+                      artist.pop + trap + indie + country + folk + rock + funk, lm_df_1)
+summary(fit.backwards)
+fit.backwards.1 <- lm(Peak.Transformed ~ danceability + duration_ms + valence +
+                        artist.pop + trap + indie + country + folk + rock + funk, lm_df_1)
+fit.backwards.2 <- lm(Peak.Transformed ~ danceability + duration_ms + valence +
+                        artist.pop + trap + country + folk + rock + funk, lm_df_1)
+summary(fit.backwards.2)
 
 # Our lm final model
-fit.final <- fit.backwards.8
+fit.final <- fit.backwards.2 
 
 # Predicted values
 yhat <- predict(fit.final, lm_df)
@@ -83,8 +87,7 @@ mse.tree.test <- mean((test_df$Peak.Position-yhat.tree.test)^2)
 # Random Forest
 library(randomForest)
 
-
-fit.rf <- randomForest(Peak.Position~., train_df, mtry=10, ntree=500) 
+fit.rf <- randomForest(Peak.Position ~., train_df, mtry=10, ntree=500) 
 
 par(mfrow=c(1,1))
 
@@ -92,21 +95,33 @@ plot(fit.rf, col="blue", pch=16, type="p", main="default plot")
 
 # stabilizes around ntree = 200, now choose mtry
 
-rf.error.p <- 1:17
+colnames(train_df)
+rf.error.p <- 1:15
 
-for (p in 1:17) {
+for (p in 1:15) {
   fit.rf <- randomForest(Peak.Position ~ ., train_df, mtry=p, ntree=200) 
   rf.error.p[p] <- fit.rf$mse[200] 
 }
 
 rf.error.p 
 
-plot(1:17, rf.error.p, pch=16, xlab="mtry",
+plot(1:15, rf.error.p[1:15], pch=16, xlab="mtry",
      ylab="OOB mse of mtry") 
-lines(1:17, rf.error.p)
+lines(1:15, rf.error.p[1:15])
 
 # choose mtry = 6 for final rf
 fit.rf.final <- randomForest(Peak.Position ~ ., train_df, mtry=6, ntree=200) 
+
+fit.rf.final <- randomForest(df2000_extra$Weeks.on.Chart[train_indices] ~ . -Peak.Position, train_df, mtry=6, ntree=200) 
+length(df2000_extra$Weeks.on.Chart)
+nrow(train_df)
+
+
+# yhat.rf.train <- predict(fit.rf.final, train_df)
+# mse.rf.train <- mean((df2000_extra$Weeks.on.Chart[train_indices]-yhat.rf.train)^2)
+# 
+# yhat.rf.test <- predict(fit.rf.final, test_df)
+# mse.rf.train <- mean((df2000_extra$Weeks.on.Chart[-train_indices]-yhat.rf.test)^2)
 
 # Testing errors
 plot(fit.rf.final$mse, xlab="number of trees", col="blue", ylab="ave mse up to i many trees using OOB predicted", pch=16)
@@ -127,8 +142,8 @@ range(yhat.rf.test)
 train_class <- train_df
 test_class <- test_df
 
-train_class$top40 <- train_df$Peak.Position <= 40
-test_class$top40 <- test_df$Peak.Position <= 40
+train_class$top40 <- as.numeric(train_df$Peak.Position <= 40)
+test_class$top40 <- as.numeric(test_df$Peak.Position <= 40)
 
 train_class$Peak.Position <- NULL
 test_class$Peak.Position <- NULL
@@ -136,24 +151,56 @@ test_class$Peak.Position <- NULL
 # Perform logistic regression
 
 library(bestglm)
-Xy <- model.matrix(top40 ~.+0 -key - liveness -tempo -time_signature, train_class) 
-Xy <- data.frame(Xy, train_class$top40)   
 
-#fit.all <- bestglm(Xy, family = binomial, method = "exhaustive", IC="AIC", nvmax = 10)
+fit.glm.all <- glm(top40 ~ ., train_class, family='binomial')
+summary(fit.glm.all)
+
+# Prune down the variables to feed into bestglm
+Xy <- model.matrix(top40 ~  danceability + duration_ms + instrumentalness + valence
+                   + Month + Season + artist.pop + trap + metal + country + Year + 
+                    liveness + 0 , train_class) 
+
+Xy <- data.frame(Xy, train_class$top40)
+
+# fit.all <- bestglm(Xy, family = binomial, method = "exhaustive", IC="AIC", nvmax = 10)
 
 fit.all$BestModel
 
-fit.glm <- glm(top40 ~ danceability + duration_ms + instrumentalness + loudness + valence
-               + Month + Season + artist.pop, train_class, family = 'binomial')
+fit.glm <- glm(top40 ~ duration_ms + instrumentalness + Month + artist.pop + Season + country + trap + Year +metal, train_class,
+               family = 'binomial')
+
 summary(fit.glm)
 
-
-pred.glm.train <- as.numeric(predict(fit.glm, train_class, type='response') >= 0.15)
+pred.glm.train <- as.numeric(predict(fit.glm, train_class, type='response') >= 0.2)
 
 mce.glm.train <- mean(pred.glm.train != train_class$top40)
 
-pred.glm.test <- as.numeric(predict(fit.glm, test_class, type='response') >= 0.15)
+pred.glm.test <- as.numeric(predict(fit.glm, test_class, type='response') >= 0.2)
 
 mce.glm.test <- mean(pred.glm.test != test_class$top40)
 
 # Classification tree
+
+tree.class <- rpart(top40 ~ ., train_class)
+pred.tree.train <- as.numeric(predict(tree.class, train_class) >= 0.10)
+mce.tree.train.class <- mean(pred.tree.train != train_class$top40)
+
+
+pred.tree.test <- as.numeric(predict(tree.class, test_class) >= 0.10)
+mce.tree.test.class <- mean(pred.tree.test != test_class$top40)
+
+
+# Classification RF with default mtry
+
+par(mfrow=c(1,1))
+
+fit.rf.class <- randomForest(as.factor(top40)  ~ ., train_class, ntree=500) 
+plot(fit.rf.class)
+
+fit.rf.pred.train <- predict(fit.rf.class, train_class, type="response") 
+mce.rf.train <- mean(train_class$top40 != fit.rf.pred.train)
+
+fit.rf.pred.test <- predict(fit.rf.class, test_class, type="response") 
+mce.rf.train <- mean(test_class$top40 != fit.rf.pred.test)
+
+
