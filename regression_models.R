afbfbf1981fdf74f173c19e2e7c016223bf297e0
Andrew Zheng <andrew.a.zheng@gmail.com>
2019-05-05 11:19:33 -0400

    Update regression_models.R

--- regression_models.R
+++ regression_models.R
@@ -5,10 +5,8 @@ lm_df <- df2000_grouped %>%
   select(-artist, -song, -WeekID, -Weeks.on.Chart)
 
 library(leaps)
-
-fit.exh <- regsubsets(Peak.Position ~., lm_df, nvmax=25, method="exhaustive")
+fit.exh <- regsubsets(Peak.Position ~ . -X, lm_df, nvmax=25, method="exhaustive")
 f.e <- summary(fit.exh)
-names(f.e)
 which.min(f.e$rss)
 data.frame(variables = (1:length(f.e$rsq)),
            r_squared = f.e$rsq,
@@ -18,14 +16,27 @@ data.frame(variables = (1:length(f.e$rsq)),
 
 fit.backward <- regsubsets(Peak.Position ~., lm_df, nvmax=10, method="backward")
 
-fit.final <- lm(Peak.Position ~ ., lm_df)
+fit.final <- lm(Peak.Position ~  -X., lm_df)
 summary(fit.final)
 
-fit.dance <- lm(log(Peak.Position) ~ log(danceability), lm_df)
+fit.dance <- lm(Peak.Position ~ danceability, lm_df)
 
-sqrt(f.e$rss[13] / nrow(lm_df))
+yhat_lm <- predict(fit.final, lm_df)
+mse.lm.train <- mean((lm_df$Peak.Position - yhat_lm)^2)
+
+yhat_lm_dance <- predict(fit.dance, lm_df)
+mse.dance.train <- mean((lm_df$Peak.Position - yhat_lm_dance)^2)
 
 library(ranger)
 library(rpart)
 
-tree.full <- rpart(Peak.Position ~ ., lm_df)
+tree.full <- rpart(Peak.Position ~ . -X, lm_df)
+
+yhat_tree <- predict(tree.full, lm_df)
+mse.tree.train <- mean((lm_df$Peak.Position-yhat_tree)^2)
+
+
+top_10 <- df2000_grouped %>%
+  filter(as.integer(Peak.Position) <= 10)
+
+
